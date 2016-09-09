67272f8f0860c4dbcbd718a5ae0242a869feb78a
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-09-09 13:50:33 -0700

    Change package system for questions directed to user.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,36 +2,21 @@
 import java.io.Console;
 
 public class App {
-  public static void main(String[] args ) {
-    Console myConsole = System.console();
+public static void main(String[] args ) {
+Console myConsole = System.console();
 
-Event packageOne = new Event(100, "Mexican", "Tequila Shots and Margaritas", "Mariachi Los Tres Potros");
-Event packageTwo = new Event(150, "Indian", "Dirty Chai Martini and Whiskey", "Dj Prashant");
-Event packageThree = new Event(200, "American", "Beers and Jello Shots", "Britney Spears");
+System.out.println("Welcome to Krishna Event Planning!");
+System.out.println("How many guests will be attending your event?");
+Int numberOfGuests = Integer.parseInt(myConsole.readline());
 
-Event[] allPackages = {packageOne, packageTwo, packageThree};
+System.out.println("If you are interested in having your event catered by us, please choose and enter the number associated with your prefered cuisine: Mexican(1), Indian(2), or American(3).");
+Int foodOfChoice = Integer.parseInt(myConsole.readline());
 
+System.out.println("Would you like to include drinks in your events? Please enter the number associated with your choice: No(0), Yes, Alcoholic and Non-Alcoholic(4), Yes, only Non-Alcoholic(1).");
+Int drinksOfChoice = Integer.parseInt(myConsole.readline());
 
+System.out.println("Our company offers the option of having live entertainment. Please enter the number associated with your choice: No entertainment(0), Rock Band(1), Dj services(2), Professional Singer with dancers(5).");
+Int entertainmentOfChoice = Integer.parseInt(myConsole.readline());
 
-System.out.println("What event package would you like?");
-
-System.out.println("Events Provided by Krishna Events:");
-
-for( Event userChoiceOfPackage : allPackages) {
-  System.out.println("_________________________");
-  System.out.println(userChoiceOfPackage.mGuests );
-  System.out.println(userChoiceOfPackage.mFood );
-  System.out.println(userChoiceOfPackage.mDrinks );
-  System.out.println(userChoiceOfPackage.mEntertainment );
-
+  }
 }
-
-String stringUserChoice = myConsole.readLine();
-System.out.println("The total cost of your event will be : ");
-
-
-
-}
-
-}
-  
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -4,6 +4,8 @@ public class Event {
   public String mFood;
   public String mDrinks;
   public String mEntertainment;
+  public int mCost = 0;
+
 
 public Event( int guests, String food, String drinks, String entertainment){
 
@@ -12,7 +14,19 @@ public Event( int guests, String food, String drinks, String entertainment){
   mDrinks = drinks;
   mEntertainment = entertainment;
 
- }
+  }
+  public int guests(int numberOfGuests) {
+    return mGuests;
+  }
 
+  public String food(int foodOfChoice) {
+    return mFood;
+  }
 
-}
+  public String drinks(int drinksOfChoice) {
+    return mDrinks;
+  }
+
+  public String entertainment(int entertainmentOfChoice) {
+    return mEntertainment;
+  }
