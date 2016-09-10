a54131d993519fcf8e21852d007c1b58787eed55
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-09-10 13:04:29 -0700

    Change primitive int to wrapper classes.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,16 +9,16 @@ public static void main(String[] args ) {
 Console myConsole = System.console();
 
 Event event = new Event(guests, food, drinks, entertainment);
-int foodCost = event.getFoodCost();
-int drinksCost = event.getDrinksCost();
-int totalEventCost = event.getTotalEventCost();
+Integer foodCost = event.getFoodCost();
+Integer drinksCost = event.getDrinksCost();
+Integer totalEventCost = event.getTotalEventCost();
 
 
 //int totalEventCost = (mGuests + (mGuests * mFood) + (mGuests * mDrinks ) + mEntertainment);
 
 System.out.println("Welcome to Krishna Event Planning!");
 System.out.println("How many guests will be attending your event? Please choose between our three options. Make sure to enter the number associated with the option: 50 guests (1) , 100 guests (2) , 200 guests (3)");
-int guests = Integer.parseInt(myConsole.readLine());
+Integer guests = Integer.parseInt(myConsole.readLine());
 if( guests == 1) {
   guests = 50;
 } else if (guests == 2) {
@@ -28,7 +28,7 @@ if( guests == 1) {
 }
 
 System.out.println("If you are interested in having your event catered by us, please choose and enter the number associated with your prefered cuisine: Mexican(1), Indian(2), or American(3).");
-int food = Integer.parseInt(myConsole.readLine());
+Integer food = Integer.parseInt(myConsole.readLine());
 if( food == 1) {
   food = 20;
 } else if (food == 2) {
@@ -38,7 +38,7 @@ if( food == 1) {
 }
 
 System.out.println("Would you like to include drinks in your events? Please enter the number associated with your choice: No(1), Yes, Alcoholic and Non-Alcoholic(2), Yes, only Non-Alcoholic(3).");
-int drinks = Integer.parseInt(myConsole.readLine());
+Integer drinks = Integer.parseInt(myConsole.readLine());
 if( drinks == 1) {
   drinks = 0;
 } else if (drinks == 2) {
@@ -48,7 +48,7 @@ if( drinks == 1) {
 }
 
 System.out.println("Our company offers the option of having live entertainment. Please enter the number associated with your choice: No entertainment(1), Rock Band(2), Dj services(3), Professional Singer with dancers(4).");
-int entertainment= Integer.parseInt(myConsole.readLine());
+Integer entertainment= Integer.parseInt(myConsole.readLine());
 if( entertainment == 1) {
   entertainment = 100;
 } else if (entertainment == 2) {
