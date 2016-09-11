7428a5c166fee18d5af29e6e67351d788fc4663a
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-09-11 15:33:04 -0700

    Change if/else if/ else statements into switch statement.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,72 +1,99 @@
+package main;
 
-//program cannot execute cost 
-
-import java.io.Console;
+import java.util.Scanner;
 
 public class App {
 public static void main(String[] args ) {
-Console myConsole = System.console();
+Scanner aScanner = new Scanner(System.in);
 
-Event event = new Event(guests, food, drinks, entertainment);
-Integer foodCost = event.getFoodCost();
-Integer drinksCost = event.getDrinksCost();
-Integer totalEventCost = event.getTotalEventCost();
-
-
-//int totalEventCost = (mGuests + (mGuests * mFood) + (mGuests * mDrinks ) + mEntertainment);
-
+//take user input for guests
 System.out.println("Welcome to Krishna Event Planning!");
-System.out.println("How many guests will be attending your event? Please choose between our three options. Make sure to enter the number associated with the option: 50 guests (1) , 100 guests (2) , 200 guests (3)");
-Integer guests = Integer.parseInt(myConsole.readLine());
-if( guests == 1) {
-  guests = 50;
-} else if (guests == 2) {
-  guests = 100;
-} else if ( guests == 3) {
-  guests = 200;
-} else {
-  System.out.println("Sorry, I didn't understand what you meant.Please choose of of the above options by providing the number associated with it.");
-}
+System.out.println("How many guests will be attending your event?");
+int guests = Integer.parseInt(aScanner.nextLine());
 
+int userInput;
+int food = 0;
+int mexicanFood = 20;
+int indianFood = 40;
+int americanFood = 75;
+//take user input for the food
+do{
 System.out.println("If you are interested in having your event catered by us, please choose and enter the number associated with your prefered cuisine: Mexican(1), Indian(2), or American(3).");
-Integer food = Integer.parseInt(myConsole.readLine());
-if( food == 1) {
-  food = 20;
-} else if (food == 2) {
-  food = 40;
-} else if ( food == 3) {
-  food = 75;
-} else {
-  System.out.println("Sorry, I didn't understand what you meant.Please choose of of the above options by providing the number associated with it.");
-}
+ userInput = Integer.parseInt(aScanner.nextLine());
 
+switch (userInput){
+case 1:   
+  food = mexicanFood;
+  break;
+case 2: 	
+ food = indianFood;
+ break;
+case 3:
+  food = americanFood;
+  break;
+default:
+  System.out.println("This is not a valid input.");
+  break;
+}
+}while(userInput<1 || userInput>3); //runs if user input is invalid, breaks once user input is valid
+
+  
+
+
+int drinksCost = 0;
+int userInputDrinks;
+//take user input for drinks
+do{
 System.out.println("Would you like to include drinks in your events? Please enter the number associated with your choice: No(1), Yes, Alcoholic and Non-Alcoholic(2), Yes, only Non-Alcoholic(3).");
-Integer drinks = Integer.parseInt(myConsole.readLine());
-if( drinks == 1) {
-  drinks = 0;
-} else if (drinks == 2) {
-  drinks = 35;
-} else if ( drinks == 3) {
-  drinks = 10;
-} else {
-  System.out.println("Sorry, I didn't understand what you meant.Please choose of of the above options by providing the number associated with it.");
+ userInputDrinks = Integer.parseInt(aScanner.nextLine());
+switch (userInputDrinks){
+case 1:
+  drinksCost = 0;
+  break;
+case 2:
+  drinksCost = 35;
+  break;
+case 3:
+  drinksCost = 10;
+  break;
+default:
+ System.out.print("This is not a valid input.Please try again");
+ break;
 }
+}while(userInputDrinks <1 || userInputDrinks>3); ////runs if user input is invalid, breaks once user input is valid
 
+
+int entertainmentCost = 0;
+int userInputEntertainment;
+
+//take user input for entertainment
+do{
 System.out.println("Our company offers the option of having live entertainment. Please enter the number associated with your choice: No entertainment(1), Rock Band(2), Dj services(3), Professional Singer with dancers(4).");
-Integer entertainment= Integer.parseInt(myConsole.readLine());
-if( entertainment == 1) {
-  entertainment = 100;
-} else if (entertainment == 2) {
-  entertainment = 200;
-} else if ( entertainment == 3) {
-  entertainment = 350;
-} else if (entertainment == 2) {
-  entertainment = 650;
-} else {
-  System.out.println("Sorry, I didn't understand what you meant.Please choose of of the above options by providing the number associated with it.");
+ userInputEntertainment= Integer.parseInt(aScanner.nextLine());
+switch (userInputEntertainment){
+case 1:
+ entertainmentCost = 100;
+ break;
+case 2:
+  entertainmentCost = 200;
+  break;
+case 3:
+  entertainmentCost = 350;
+  break;
+  
+case 4:
+  entertainmentCost = 650;
+  break;
+default:
+ System.out.print("This is not a valid input.");
+ break;
 }
+}while(userInputEntertainment<1 || userInputEntertainment>4); //runs if user input is invalid, breaks once user input is valid
 
-System.out.println("The total cost of your desired event is:/n" + "$" +  totalEventCost);
+Event event = new Event(guests, food, drinksCost, entertainmentCost);
+int totalEventCost = event.getTotalEventCost();
+
+System.out.println("The total cost of your desired event is: " + "$" +  totalEventCost);
 
  }
 }
