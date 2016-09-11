e2e0a4d9a15711f4abf202e4da776a4c43f1e381
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-09-11 15:39:02 -0700

    Add code for different test.

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,11 +1,22 @@
-import org.junit.*;
+package test;
+
 import static org.junit.Assert.*;
+import main.Event;
+
+import org.junit.Test;
 
 public class EventTest {
 
-@Test
-  public void isEvent_forMoreThanTwentyPeople_true(){
-    Event customerEvent  = new Event();
-    assertEquals(true, customerEvent.isEvent(25));
+	@Test
+	public void test() {
+		int guests = 10;
+		int food = 20;
+		int drinks = 35;
+		int entertainment = 350;
+		
+  Event event = new Event( guests, food, drinks, entertainment);
+	assertEquals(900, event.getTotalEventCost()); 
+	System.out.println(event.getTotalEventCost());
   }
+
 }
